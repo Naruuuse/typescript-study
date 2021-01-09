044108345b75cb9ca3c2d77ed4e6c95f2c71ede7
naruse <kuroraido@outlook.jp>
2021-01-10 02:30:28 +0900

    2-21

--- app.ts
+++ app.ts
@@ -1,35 +1,15 @@
-// const person: {
-//   name: string
-//   age: number
-//   hobbies: string[]
-//   role: [number, string]
-// } = {
-//   name: 'yota',
-//   age: 30,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, 'author']
-// }
-
-enum Role {
-  ADMIN, READ_ONLY, AUTHOR
+function combine(input1: number | string, input2: number | string) {
+  let result
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2
+  } else {
+    result = input1.toString() + input2.toString()
+  }
+  return result
 }
 
-const person = {
-  name: 'yota',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN
-}
+const combineAges = combine(30, 26)
+console.log(combineAges)
 
-let favoriteActivities: string[]
-favoriteActivities = ['Sports']
-
-console.log(person)
-
-for (const hobby of person.hobbies) {
-  console.log(hobby)
-}
-
-if (person.role === Role.ADMIN) {
-  console.log('管理者ユーザ')
-}
+const combineNames = combine('Max', 'Anna')
+console.log(combineNames)
